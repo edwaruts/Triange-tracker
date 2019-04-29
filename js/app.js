834000aa6bd0336b48b4246184655d63e23c3f90
edwaruts <edwarutere@gmail.com>
2019-04-29 09:55:44 +0300

    function track

--- js/app.js
+++ js/app.js
@@ -25,10 +25,10 @@ function triangle() {
 
 function track(a, b, c) {
     if (a + b > c && b + c > a && c + a > b) {
-        if (a === b && a === c) {
+        if (a == b && a == c) {
             display.innerHTML =
                 "<p class='display-text'>That is an <span class='type'>Equilateral</span> triangle<p><span class='fill'>ALL</span> sides are equal";
-        } else if (a === b || b === c || a === c) {
+        } else if (a == b || b == c || a == c) {
             display.innerHTML =
                 "<p class='display-text'>That is an <span class='type'>Isosceles</span> triangle<p><span class='fill'>TWO</span> sides are equal";
         } else {
