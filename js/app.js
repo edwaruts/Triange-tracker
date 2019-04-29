e225b4407a881e366877b7b96a5d044ecbe4f793
edwaruts <edwarutere@gmail.com>
2019-04-29 06:22:20 +0300

    fixed js function track

--- js/app.js
+++ js/app.js
@@ -4,26 +4,26 @@ function triangle() {
     //push all the sides into the array
     sides.push(document.getElementById("num1").value);
     sides.push(document.getElementById("num2").value);
-    sides.push(document.getElementById("num3).value);
-            //assign the values of the array to variables a,b,c
-            var [a, b, c] = sides;
-            var display = document.getElementById("display");
-            var explanation = document.getElementById("explanation");
-            if (a.length === 0 || b.length === 0 || c.length === 0) {
-                //check whether the user has typed something
-                display.innerHTML =
-                    "<p>Please <span class='fill'>FILL</span> all the fields</p>";
-            } else {
-                //convert the strings to numbers
-                a = parseFloat(a);
-                b = parseFloat(b);
-                c = parseFloat(c);
-                //track the triangle
-                tracka, b, c);
-        }
+    sides.push(document.getElementById("num3").value);
+    //assign the values of the array to variables a,b,c
+    var [a, b, c] = sides;
+    var display = document.getElementById("display");
+    var explanation = document.getElementById("explanation");
+    if (a.length === 0 || b.length === 0 || c.length === 0) {
+        //check whether the user has typed something
+        display.innerHTML =
+            "<p>Please <span class='fill'>FILL</span> all the fields</p>";
+    } else {
+        //convert the strings to numbers
+        a = parseFloat(a);
+        b = parseFloat(b);
+        c = parseFloat(c);
+        //track the triangle
+        track(a, b, c);
     }
+}
 
-    function tracka, b, c) {
+function track(a, b, c) {
     if (a + b > c && b + c > a && c + a > b) {
         if (a === b && a === c) {
             display.innerHTML =
